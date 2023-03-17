39af06169131678d30b9de3d6b6b741f52c922fc
aozp73 <aozp73@naver.com>
2023-03-17 16:15:04 +0900

    initial commit

---
+++ src/main/java/shop/mtcoding/filter/FilterApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilterApplication.class, args);
+	}
+
+}
